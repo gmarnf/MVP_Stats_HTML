0924e1ed357a102e56414784a84d9c9a627382d0
gmarnf <gilles.stats@gmail.com>
2025-11-11 12:03:18 +0100

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,13 +1,32 @@
 // Initialisation de l’interface et des actions
 
 function initUI() {
+  // Remplir l’UI principale
   renderCadre();
+  renderActifsSelector(joueurs);
 
+  // Charger valeurs de période/score depuis localStorage si dispo
+  const savedPeriode = loadLocal("periodeValue", null);
+  const savedScore = loadLocal("scoreValue", null);
+  if (savedPeriode) getEl("periodeSelect").value = savedPeriode;
+  if (savedScore) getEl("scoreInput").value = savedScore;
+
+  // Écoute pour sauvegarder période/score
+  getEl("periodeSelect").addEventListener("change", (e) => {
+    saveLocal("periodeValue", e.target.value);
+  });
+  getEl("scoreInput").addEventListener("input", (e) => {
+    saveLocal("scoreValue", e.target.value);
+  });
+
+  // Boutons journal
   const btnVoir = getEl("btnVoirJournal");
   const btnCSV = getEl("btnExportCSV");
+  const btnReset = getEl("btnResetJournal");
 
   if (btnVoir) btnVoir.addEventListener("click", afficherJournalHTML);
   if (btnCSV) btnCSV.addEventListener("click", exporterJournalCSV);
+  if (btnReset) btnReset.addEventListener("click", resetJournal);
 }
 
 document.addEventListener("DOMContentLoaded", initUI);
